a335638fb813b38e3a35e5afb2388a90694325a2
Emad Ibrahim <eibrahim@gmail.com>
2025-03-10 12:37:35 -0400

    added google analytics

--- src/app/(saas)/layout.tsx
+++ src/app/(saas)/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function LandingLayout({
@@ -16,6 +17,18 @@ export default function LandingLayout({
           name="description"
           content="The open-source intelligent calendar that adapts to your workflow. Experience seamless task scheduling powered by AI, designed to make your time management effortless."
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <ThemeProvider
